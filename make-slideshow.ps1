5adb3b4766da0fc5b8f0b7a7bcb5aca3b7fe7c20
malin-bg <252877943+malin-bg@users.noreply.github.com>
2026-03-16 14:09:16 +0200

    gallery

--- make-slideshow.ps1
+++ make-slideshow.ps1
@@ -1,4 +1,13 @@
-$files = Get-ChildItem -Path ".\kolaji\full" -File | Sort-Object Name
+$files = Get-ChildItem -Path ".\kolaji\full" -File |
+  Where-Object { $_.Extension -match '^\.(jpg|jpeg|png|webp)$' } |
+  Sort-Object Name
+
 $paths = $files | ForEach-Object { "/kolaji/full/$($_.Name)" }
+
+# gallery: always all images
+$paths | ConvertTo-Json | Set-Content -Encoding UTF8 ".\slideshow\gallery.json"
+Write-Host "Updated slideshow/gallery.json with $($files.Count) images."
+
+# slideshow: start as full list (you can manually delete rows later)
 $paths | ConvertTo-Json | Set-Content -Encoding UTF8 ".\slideshow\slideshow.json"
-Write-Host "Updated slideshow/slideshow.json with $($files.Count) items."
+Write-Host "Updated slideshow/slideshow.json with $($files.Count) images."
