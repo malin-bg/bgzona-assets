27dfba43cfc0a82e821307f8b270ef987c91307f
malin-bg <252877943+malin-bg@users.noreply.github.com>
2026-03-16 16:26:40 +0200

    make-slideshow.ps1-

--- make-slideshow.ps1
+++ make-slideshow.ps1
@@ -4,10 +4,21 @@ $files = Get-ChildItem -Path ".\kolaji\full" -File |
 
 $paths = $files | ForEach-Object { "/kolaji/full/$($_.Name)" }
 
+$excludeFile = ".\slideshow\slideshow-exclude.json"
+
+# Load excludes if file exists, otherwise use empty array
+if (Test-Path $excludeFile) {
+    $excluded = Get-Content $excludeFile -Raw | ConvertFrom-Json
+} else {
+    $excluded = @()
+}
+
 # gallery: always all images
 $paths | ConvertTo-Json | Set-Content -Encoding UTF8 ".\slideshow\gallery.json"
 Write-Host "Updated slideshow/gallery.json with $($files.Count) images."
 
-# slideshow: start as full list (you can manually delete rows later)
-$paths | ConvertTo-Json | Set-Content -Encoding UTF8 ".\slideshow\slideshow.json"
-Write-Host "Updated slideshow/slideshow.json with $($files.Count) images."
+# slideshow: all except excluded
+$slideshowPaths = $paths | Where-Object { $_ -notin $excluded }
+
+$slideshowPaths | ConvertTo-Json | Set-Content -Encoding UTF8 ".\slideshow\slideshow.json"
+Write-Host "Updated slideshow/slideshow.json with $($slideshowPaths.Count) images."
